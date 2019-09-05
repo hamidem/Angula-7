97c75343a8436ed7fd9dc45536fd7c57d22e1033
hamidem <33391305+hamidem@users.noreply.github.com>
2019-09-05 14:23:22 +0200

    Add files via upload

---
+++ src/app/shared/data/medias.ts
@@ -0,0 +1,28 @@
+export class Medias {
+
+    static actuFeedList: Array<string> = [
+        'http://www.lepoint.fr/24h-infos/rss.xml',
+        'http://www.lemonde.fr/m-actu/rss_full.xml',
+        'http://www.lexpress.fr/rss/alaune.xml',
+        'http://www.courrierinternational.com/feed/category/6268/rss.xml',
+        'http://www.la-croix.com/RSS/UNIVERS'
+    ];
+
+    static scienceFeedList: Array<string> = [
+        'http://www.lemonde.fr/sciences/rss_full.xml',
+        'http://www.lexpress.fr/rss/science-et-sante.xml'
+    ];
+
+    static cultureFeedList: Array<string> = [
+        'http://www.lemonde.fr/culture/rss_full.xml',
+        'http://www.lepoint.fr/culture/rss.xml',
+        'http://www.courrierinternational.com/feed/category/6270/rss.xml'
+    ];
+
+    static sportFeedList: Array<string> = [
+        'http://www.lepoint.fr/sport/rss.xml',
+        'https://www.lemonde.fr/sport/rss_full.xml',
+        'http://www.lequipe.fr/rss/actu_rss.xml',
+        'http://www.sports.fr/fr/cmc/rss.xml'
+    ];
+}
